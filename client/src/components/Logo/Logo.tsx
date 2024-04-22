65fd85358664e2c253b8d54a8413125c8daca544
jotagadev <henriquesoaresj2@gmail.com>
2024-04-22 04:26:33 -0300

    fix: correcao fonte

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,10 +1,15 @@
 import styles from "./logo.module.css";
+import { League_Spartan } from "next/font/google";
+
+const league = League_Spartan({ subsets: ['latin'] })
 
 export default function Logo() {
   return (
+    <div className={league.className}>
     <div className={styles.container}>
       <h1 className={styles.titulo1}>Pix</h1>
       <h1 className={styles.titulo2}>hub</h1>
+    </div>
     </div>
   );
 }
